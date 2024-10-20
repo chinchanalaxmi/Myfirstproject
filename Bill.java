768c9e38f446033e4697771a102f84ebd5d1e71d
chinchanalaxmi <chinchanalaxmip@gmail.com>
2024-10-20 14:15:24 +0530

    Bill.java

--- Bill.java
+++ Bill.java
@@ -1,57 +1,87 @@
 package codeacademy;
+import java.util.Scanner;
 
 class Bill {
-    private int price;
-    private Boolean veg;
     private int extraCheesePrice = 100;
     private int extraToppingsPrice = 150;
     private int backPackPrice = 20;
-    private int basePizzaPrice;
-    private Boolean isExtraCheeseAdded = false;
-    private Boolean isExtraToppingsAdded = false;
-    private Boolean isOptedForTakeAway = false;
 
-    public Bill(Boolean veg) {
-        this.veg = veg;
-        if (this.veg) {
-            this.price = 300;
-        } else {
-            this.price = 400;
-        }
-        basePizzaPrice = this.price;
-    }
-
+    String add = "";
+    String toppings = "";
+    String take = "";
+    Scanner scanner = new Scanner(System.in);  
     public void addExtraCheese() {
-        isExtraCheeseAdded = true;
-        System.out.println("Extra cheese added");
-        this.price += extraCheesePrice;
+        while (true) {  
+            System.out.println("Do you want extra cheese? (Yes/No)");
+            String Cheese = scanner.nextLine().trim().toLowerCase();  
+            
+            if (Cheese.equals("yes")) {
+                add = "Added";
+                System.out.println("Extra cheese: Added");
+                break;  
+            } else if (Cheese.equals("no")) {
+                add = "Not Added";
+                System.out.println("Extra cheese: Not Added");
+                break;  
+            } else {
+                System.out.println("Invalid choice, please try again.");
+            }
+        }
     }
 
+    
     public void addExtraToppings() {
-        isExtraToppingsAdded = true;
-        System.out.println("Extra Toppings added");
-        this.price += extraToppingsPrice;
+        while (true) {  
+            System.out.println("Do you want extra toppings? (Yes/No)");
+            String Toppings = scanner.nextLine().trim().toLowerCase();  
+            if (Toppings.equals("yes")) {
+                toppings = "Added";
+                System.out.println("Extra toppings: Added");
+                break; 
+            } else if (Toppings.equals("no")) {
+                toppings = "Not Added";
+                System.out.println("Extra toppings: Not Added");
+                break;  
+            } else {
+                System.out.println("Invalid choice, please try again.");
+            }
+        }
     }
 
     public void takeAway() {
-        isOptedForTakeAway = true;
-        System.out.println("Take away opted");
-        this.price += backPackPrice;
-    }
+        while (true) {  
+            System.out.println("Do you want take-away? (Yes/No)");
+            String Take = scanner.nextLine().trim().toLowerCase();  
 
-    public void getBill() {
-        String bill = "";
-        bill += "Base Pizza Price: " + basePizzaPrice + "\n";
-        if (isExtraCheeseAdded) {
-            bill += "Extra cheese added: " + extraCheesePrice + "\n";
+            if (Take.equals("yes")) {
+                take = "Take Away";
+                System.out.println("Take away: Yes");
+                break;  
+            } else if (Take.equals("no")) {
+                take = "Not Required";
+                System.out.println("Take away: No");
+                break;  
+            } else {
+                System.out.println("Invalid choice, please try again.");
+            }
+            
         }
-        if (isExtraToppingsAdded) {
-            bill += "Extra Toppings added: " + extraToppingsPrice + "\n";
+    }
+    
+    public void getBill(int PizzaPrice) {
+        if (!add.equals("Added") && !toppings.equals("Added") && !take.equals("Take Away")) {
+            System.out.println("Your Total Bill: " + PizzaPrice + " Rs");
+        } else {
+            if (add.equals("Added")) {
+                PizzaPrice += extraCheesePrice;
+            }
+            if (toppings.equals("Added")) {
+                PizzaPrice += extraToppingsPrice;
+            }
+            if (take.equals("Take Away")) {
+                PizzaPrice += backPackPrice;
+            }
+            System.out.println("Your Total Bill: " + PizzaPrice + " Rs");
         }
-        if (isOptedForTakeAway) {
-            bill += "Take away: " + backPackPrice + "\n";
-        }
-        bill += "Total Bill: " + this.price + "\n";
-        System.out.println(bill);
     }
 }
